57e3f7aa326815f62b936ddbf05d7e42b461e2c4
Yulia <yulia.blyndiuk@gmail.com>
2017-06-30 11:23:53 +0300

    added filters feature, added their selection feature

--- public/app.js
+++ public/app.js
@@ -1,19 +1,34 @@
 var state = {
-	page: 'welcome'
+	page: 'welcome',
+	filters: []
 }
 
 function showPage(page) {
 	state.page = page;
 }
 
+function selectFilter(filter) {
+	state.filters.push(filter);
+}
+
 function render() {
 	$('.page').removeClass('current');
 	$('.page#' + state.page).addClass('current');
+
+	state.filters.forEach(function(filter) {
+		$(`#filters li[data-name=${ filter }]`).addClass('selected');
+	})
 }
 
 function addListeners() {
 	$('#startButton').click(function() {
 		showPage('search');
+		render();
+	})
+
+	$('#filters li').click(function() {
+		var filterName = $(this).data('name');
+		selectFilter(filterName);
 		render();
 	})
 }
